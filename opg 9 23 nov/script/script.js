ef5aee5fa0f5bd5d467d6c94257fd4d06f4e86a0
OHoltan <ola.holtan2303@gmail.com>
2022-12-05 08:34:31 +0100

    commit 2

--- opg 9 23 nov/script/script.js
+++ opg 9 23 nov/script/script.js
@@ -31,3 +31,5 @@ restpar("hei", "fyr", "hest", 7, "knut")
 // Arrow functions don't have their own bindings to this, arguments, or super, and should not be used as methods.
 // Arrow functions cannot be used as constructors. Calling them with new throws a TypeError. They also don't have access to the new.target keyword.
 // Arrow functions cannot use yield within their body and cannot be created as generator functions.
+
+//
